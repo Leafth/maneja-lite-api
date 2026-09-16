02ca7b2710c42ec09bc4b7521ae2cb8de80089ef
Felipe Lima <lipezera1836@gmail.com>
2026-09-16 17:31:05 -0300

    chore: configura estrutura inicial da API com Express e Swagger

---
+++ src/config/swagger.js
@@ -0,0 +1,24 @@
+import swaggerJsdoc from "swagger-jsdoc";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+
+    info: {
+      title: "MANEJA Lite API",
+      version: "1.0.0",
+      description:
+        "Documentação da API simplificada do MANEJA para a disciplina de Desenvolvimento de Software para Dispositivos Móveis",
+    },
+
+    servers: [
+      {
+        url: "http://localhost:3000/api",
+      },
+    ],
+  },
+
+  apis: ["./src/routes/*.js"],
+};
+
+export const swaggerSpec = swaggerJsdoc(options);
