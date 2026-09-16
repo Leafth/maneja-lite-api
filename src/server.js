ed9e618bb834f450899291edfe934e8685f4e80e
Felipe Lima <lipezera1836@gmail.com>
2026-09-16 17:44:21 -0300

    chore: configura Sequelize com SQLite

--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
 import dotenv from "dotenv/config.js";
+import sequelize from "./config/database.js";
 import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+try {
+  await sequelize.authenticate();
+
+  console.log("Conectado ao banco de dados.");
+
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+  });
+} catch (error) {
+  console.error("Erro ao iniciar o servidor:", error);
+}
