a65709bb3bf225e43d4837cf285e1936fd3b37e7
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-09-19 23:51:15 -0300

    feat: adiciona schema e migration de usuários

---
+++ prisma/migrations/20260920022512_create_usuarios/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Usuario" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Usuario_email_key" ON "Usuario"("email");
