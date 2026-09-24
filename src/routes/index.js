f0260bd199628cd1ca72832b177081f8847cda18
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2026-09-23 21:23:13 -0300

    feat: adiciona rotas para gerenciamento de grupos

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import authRoutes from "./auth.routes.js";
+import grupoRoutes from "./grupo.routes.js";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
+router.use("/grupos", grupoRoutes);
 
 export default router;
